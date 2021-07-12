fee5f7f4588ae390aaf749333f65f9cc7af315c2
MIljan Kekovic <mkekovic@isladner.tamucc.edu>
2021-07-12 11:53:53 -0500

    Implemented Subscription (not working)
    Data is pulled but for some reason its empty

--- src/components/Metrics.js
+++ src/components/Metrics.js
@@ -2,17 +2,19 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import '../styles/Metrics.css'
 // import { actions } from '../reducer';
-import { Provider, createClient, useQuery } from 'urql';
+import { Provider, createClient, useQuery, useSubscription } from 'urql';
 import Chip from '../components/Chip';
 import Demo from '../components/Chart'
 import DropDown from '../components/dropDown'
 import MetricCard from '../components/metricCard'
+import Button from '@material-ui/core/Button';
+
 
 const client = createClient({
     url: 'https://react.eogresources.com/graphql',
 });
 
-let data = [];
+let chartData = [];
 
 const getMetrics = `
 query {
@@ -25,6 +27,7 @@ query($metricData: MeasurementQuery) {
     getMeasurements(input: $metricData) {
         value
         at
+        unit
   }
 }
 `;
@@ -36,6 +39,16 @@ query($selectedMetric: String!) {
   }
 }
 `;
+
+const GET_SUB = `
+subscription  {
+    newMeasurement {
+        value
+        metric
+  }
+}
+`;
+
 
 
 const Metrics = () => {
@@ -56,30 +69,77 @@ const Metrics = () => {
     const [resultMeasurements] = useQuery({
         query: getMeasurements,
         variables: {
-            metricData,
+            metricData
         },
     });
+    
+    const [resSub] = useSubscription({ query: GET_SUB });
+    console.log('----->', resSub)
+    // const handleSubscription = (messages = [], response) => {
+    //     return [response.newMessages, ...messages];
+    //   };
+      
+    // const Messages = () => {
+    //     const [res] = useSubscription({ query: GET_SUB }, handleSubscription);
+    //     console.log('-------------',res)
+      
+    //     if (!res.data) {
+    //       return <p>No new messages </p>;
+    //     }
+      
+    //     return (
+    //       <ul>
+    //         {res.data.map(message => (
+    //           <p key={message.id}>
+    //             {message.from}: "{message.text}"
+    //           </p>
+    //         ))}
+    //       </ul>
+    //     );
+    // };
+
+    // const NewMetricValueNotification = () => {
+    //     const { data, error, loading } = useSubscription({query: GET_SUB});
+    //     // 
+    //     console.log("--------->",data)
+    //     // if (loading) {
+    //     //     return <div> Loading.... </div>;
+    //     // }
+    //     // if (error) {
+    //     //     return <div> Error.... {error.message} </div>;
+    //     // }
+    //     // return (
+    //     //     <div>
+    //     //         <h1>New Metric Update! </h1>
+    //     //     </div>
+    //     // )
+    // }
+
+    // console.log(resultSub)
     // const resultMeasurementValue = useQuery({
     //     query: getLastMeasurement,
     //     variables: {
     //         selectedMetric,
     //     },
-    // });
-    data = resultMeasurements.data?.getMeasurements.map(({ at, value }) => {
+    // });    <div>----{Messages()}</div>
+    chartData = resultMeasurements.data?.getMeasurements.map(({ at, value }) => {
         let d = new Date(at);
         let rObj = { argument: d, value: value }
         return rObj
     })
-    var test = resultMetrics.data?.getMetrics
+    const yLabel = resultMeasurements.data?.getMeasurements[0].unit
+    var ddlData = resultMetrics.data?.getMetrics
 
     return (
         <div>
             <div className="metric-header">
                 {metric !== '' ? <MetricCard metric={metric} metricValue={resultMeasurements.data?.getMeasurements[resultMeasurements.data?.getMeasurements.length - 1].value} /> : null}
-                <DropDown list={test || []} parentCallback={changeMetric} />
+                <DropDown list={ddlData || []} parentCallback={changeMetric} />
             </div>
+           
 
-            {metric !== '' ? <Demo title={data || []} /> : null}
+            
+            {metric !== '' ? <Demo title={chartData || []}  yLabel={yLabel}/> : null}
 
         </div >
     )
